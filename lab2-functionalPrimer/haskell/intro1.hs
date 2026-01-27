4d3f083bd95c2cd5f92c059f42bf5641106f6c89
BRIAston <240117751@aston.ac.uk>
2026-01-27 11:13:54 +0000

    Comments to intro1.hs

--- lab2-functionalPrimer/haskell/intro1.hs
+++ lab2-functionalPrimer/haskell/intro1.hs
@@ -7,8 +7,8 @@ r2 = b^2 + 1/b where b = 2
 -- define a function:
 diff a b = abs (a - b)
 
-r3 = diff (diff a a) a  where a = 1
-r4 = diff (diff a b) b  where a = 1; b = 1
+r3 = diff (diff a a) a  where a = 5
+r4 = diff (diff a b) b  where a = 5; b = 3
 
 -- lists by enumeration
 r5 = [1,3,4,1]
@@ -16,11 +16,13 @@ r6 = [1..5]
 
 inc n = n + 1
 
+-- map applies function 'inc' to each element in the list.
 r7 = map inc [1..3]
 r8 = map (diff 2) [1..3]
 r9 = map sqrt [1..3]
 
-r10 = zip [1..3] (map sqrt [1..3])
+--zip combines elements of two lists into a list of 2-tuples.
+r10 = zip[1..3] (map sqrt [1..3])
 r11 = zip [1..3] (map sqrt [1..2])
 
 r12 = print [1..3]
